ea2e2af1d20f6611d21aa4af68ba5526b3fa7668
yousufasi97 <yousufasi97@gmail.com>
2025-05-28 20:10:08 -0500

    Data Transformation completed

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
+from networksecurity.components.data_transformation import DataTransformation
+
 
 if __name__=='__main__':
     try:
@@ -22,5 +24,11 @@ if __name__=='__main__':
         data_validation_artifact=data_validation.initiate_data_validation()
         logging.info("Data Validation Completed")
         print(data_validation_artifact)
+        data_transformation_config=DataTransformationConfig(trainingpipelineconfig)
+        logging.info("data Transformation started")
+        data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
+        data_transformation_artifact=data_transformation.initiate_data_transformation()
+        print(data_transformation_artifact)
+        logging.info("data Transformation completed")
     except Exception as e:
         raise NetworkSecurityException(e,sys)
--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -26,3 +26,31 @@ def write_yaml_file(file_path:str,content:object,replace:bool = False)->None:
     except Exception as e:
         raise NetworkSecurityException(e,sys)
 
+def save_numpy_array_data(file_path:str , array:np.array):
+    """
+    save numpy array data to file
+    file_parth:str location where the file should be saved
+    array:np.array data to save
+    
+    """
+
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path,exist_ok=True)
+
+        with open(file_path,"wb") as file_obj:
+            np.save(file_obj,array)
+    except Exception as e:
+        raise NetworkSecurityException(e,sys) from e
+    
+
+def save_object(file_path:str, obj:object) ->None:
+    try:
+        logging.info("Entered the save_object method of MainUtils class")
+        os.makedirs(os.path.dirname(file_path),exist_ok=True)
+        with open(file_path,"wb") as file_obj:
+            pickle.dump(obj,file_obj)
+        logging.info("Exited the save_object method of MainUtils class")
+    except Exception as e:
+        raise NetworkSecurityException(e,sys) from e
+                    
