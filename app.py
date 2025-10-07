ceac3512e779779299c1a0888f68a974192a82d3
yousufasi97 <yousufasi97@gmail.com>
2025-10-07 17:33:53 -0500

    final commit

--- app.py
+++ app.py
@@ -85,4 +85,4 @@ async def predict_route(request:Request,file:UploadFile=File(...)):
     except Exception as e:
             raise NetworkSecurityException(e,sys)
 if __name__ == "__main__":
-    app_run(app,host="localhost",port=8000)
+    app_run(app,host="0.0.0.0",port=8000)
